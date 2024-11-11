913e042ae3b3725795da279cf1f3d39ae0f9ef05
firzandiads <firzandi200803@gmail.com>
2024-11-11 10:11:27 +0700

    Slicing Design Second Row

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.play.services.mlkit.barcode.scanning)
+    implementation(libs.flexbox)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/com/example/ptamanah/view/settings/TambahBanner.kt
@@ -0,0 +1,41 @@
+package com.example.ptamanah.view.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.GravityCompat
+import com.example.ptamanah.R
+import com.google.android.material.appbar.MaterialToolbar
+import androidx.drawerlayout.widget.DrawerLayout
+
+class TambahBanner : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_tambah_banner, container, false)
+
+        // Initialize the Toolbar
+        val toolbar = view.findViewById<MaterialToolbar>(R.id.topAppBarBanner)
+
+        // Initialize the DrawerLayout
+        val drawerLayout = activity?.findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        // Set navigation click listener to open/close the drawer
+        toolbar.setNavigationOnClickListener {
+            if (drawerLayout?.isDrawerOpen(GravityCompat.START) == true) {
+                drawerLayout.closeDrawer(GravityCompat.START)
+            } else {
+                drawerLayout?.openDrawer(GravityCompat.START)
+            }
+        }
+
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+}
