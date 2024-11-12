a3afdb5baeca3505206ec24941be8f4b6b31389f
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-11-13 00:08:54 +0700

    tambahin button listener

--- app/src/main/java/com/example/ptamanah/view/settings/rekening/RekeningFragment.kt
+++ app/src/main/java/com/example/ptamanah/view/settings/rekening/RekeningFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.example.ptamanah.R
+import com.example.ptamanah.view.settings.halaman.PreviewFragment
 
 class RekeningFragment : Fragment() {
 
@@ -13,7 +16,33 @@ class RekeningFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_rekening, container, false)
+        val view = inflater.inflate(R.layout.fragment_rekening, container, false)
+
+        val tambahRekeningButton = view.findViewById<Button>(R.id.TambahRekening)
+        tambahRekeningButton.setOnClickListener {
+            navigateToTambahRekeningFragment()
+        }
+
+        val editButton = view.findViewById<ImageView>(R.id.EditRekening)
+        editButton.setOnClickListener {
+            navigateToEditRekening()
+        }
+
+        return view
+    }
+
+    private fun navigateToEditRekening() {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.containerToko, EditRekeningFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+    private fun navigateToTambahRekeningFragment() {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.containerToko, TambahRekeningFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
     }
 
 
