fc4c4daa8f4122ddfa6d685056a9325b66853725
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-11-13 00:08:13 +0700

    memanggil all fragment dalam host fragment

--- app/src/main/java/com/example/ptamanah/view/settings/halaman/MediaSosialTokoFragment.kt
+++ app/src/main/java/com/example/ptamanah/view/settings/halaman/MediaSosialTokoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.ptamanah.R
 import com.example.ptamanah.databinding.FragmentMediaSosialTokoBinding
 
 class MediaSosialTokoFragment : Fragment() {
@@ -16,6 +17,16 @@ class MediaSosialTokoFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMediaSosialTokoBinding.inflate(inflater, container, false)
+
+        binding.titleMediaSosial.setOnClickListener {
+            if (binding.containerMediaSosial.visibility == View.GONE) {
+                binding.containerMediaSosial.visibility = View.VISIBLE
+                binding.iconArrow.setImageResource(R.drawable.baseline_keyboard_arrow_down_24)
+            } else {
+                binding.containerMediaSosial.visibility = View.GONE
+                binding.iconArrow.setImageResource(R.drawable.baseline_keyboard_arrow_up_24)
+            }
+        }
 
         return binding.root
     }
--- app/src/main/java/com/example/ptamanah/view/settings/halaman/TokoFragment.kt
+++ app/src/main/java/com/example/ptamanah/view/settings/halaman/TokoFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.FrameLayout
+import android.widget.TextView
 import com.example.ptamanah.R
 import com.example.ptamanah.databinding.FragmentTokoBinding
 import com.google.android.material.textfield.MaterialAutoCompleteTextView
@@ -25,6 +27,16 @@ class TokoFragment : Fragment() {
         val adapter = ArrayAdapter(requireContext(), android.R.layout.simple_list_item_1, locationsArray)
 
         (binding.textFieldProvinsi.editText as? MaterialAutoCompleteTextView)?.setAdapter(adapter)
+
+        binding.titleToko.setOnClickListener {
+            if (binding.containerTokoContent.visibility == View.GONE) {
+                binding.containerTokoContent.visibility = View.VISIBLE
+                binding.iconArrow.setImageResource(R.drawable.baseline_keyboard_arrow_down_24)
+            } else {
+                binding.containerTokoContent.visibility = View.GONE
+                binding.iconArrow.setImageResource(R.drawable.baseline_keyboard_arrow_up_24)
+            }
+        }
 
         return binding.root
     }
