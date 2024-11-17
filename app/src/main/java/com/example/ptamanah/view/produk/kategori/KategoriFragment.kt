5a7b451a2bbd8f3fb3c8b04406515acf862d984c
Alfeus Martinus Sinurat <feusmartinus@gmail.com>
2024-11-17 19:53:07 +0700

    slicing kategori

--- app/src/main/java/com/example/ptamanah/view/produk/kategori/KategoriFragment.kt
+++ app/src/main/java/com/example/ptamanah/view/produk/kategori/KategoriFragment.kt
@@ -6,55 +6,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.ptamanah.R
+import com.example.ptamanah.databinding.FragmentDaftarBannerBinding
+import com.example.ptamanah.databinding.FragmentKategoriBinding
+import com.example.ptamanah.view.settings.banner.TambahBanner
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [KategoriFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class KategoriFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentKategoriBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kategori, container, false)
+        _binding = FragmentKategoriBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment KategoriFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            KategoriFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnTambahKategori.setOnClickListener {
+            navigateToTambahKategoriFragment()
+        }
     }
+
+    private fun navigateToTambahKategoriFragment() {
+        val transaction = parentFragmentManager.beginTransaction()
+        transaction.replace(R.id.containerToko, TambahKategoriFragment())
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
 }
